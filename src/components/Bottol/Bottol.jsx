2468c62a4097f07e41d1ce01e41791f60fd3434e
programming hero <meherun2002@gmail.com>
2026-02-10 10:59:18 +0600

    data added to local storage and then back to cart

---
+++ src/components/Bottol/Bottol.jsx
@@ -0,0 +1,14 @@
+import './Bottol.css'
+const Bottol = ({bottol,handleAddToCart}) => {
+    const {name,img,price}=bottol;
+    return (
+        <div className="bottol">
+            <h3>Bottol:{name}</h3>
+            <img src={img} alt="" />
+            <p>Price;${price}</p>
+            <button onClick={()=>handleAddToCart(bottol)}>Purchase</button>
+        </div>
+    );
+};
+
+export default Bottol;
---
+++ src/components/Bottols/Bottols.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import Bottol from "../Bottol/Bottol";
+import './Bottols.css'
+import { addToLS, getStoredCart } from "../../Utilities/localstorage";
+const Bottols = () => {
+    const [bottols,setBottols]=useState([])
+    const [cart,setCart]=useState([]);
+    useEffect(()=>{
+        fetch('bottol.json')
+        .then(res=>res.json())
+        .then(data=>setBottols(data))
+    },[])
+    //load cart from local storage
+    useEffect(()=>{
+        if(bottols.length>0){
+            const storedCart=getStoredCart();
+        console.log(storedCart)
+        }
+    },[bottols])
+    const handleAddToCart=bottol=>{
+        const newCart=[...cart,bottol];
+        setCart(newCart);
+        addToLS(bottol.id);
+    }
+    return (
+        <div>
+         <h2>Bottols avaiable:{bottols.length}</h2>  
+         <h2>Cart:{cart.length}</h2>  
+        <div className="bottol-container">
+             {
+            bottols.map(bottol=><Bottol key={bottol.id}bottol={bottol} handleAddToCart={handleAddToCart}></Bottol>)
+         } 
+        </div>
+        </div>
+    );
+};
+
+export default Bottols;
